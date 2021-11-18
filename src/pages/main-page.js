0de10b158f0d933c63b30e9fb007a61fc66fa711
jaanmolder <jaan.molder@outlook.com>
2021-11-18 20:04:48 +0200

    Update main-page.js

--- src/pages/main-page.js
+++ src/pages/main-page.js
@@ -7,10 +7,12 @@ import SectionFive from "./sections/section-five";
 import { useEffect, useState } from "react";
 import deviceIsMobile from "./components/device-is-mobile";
 import { getItem } from "../actions";
+import ShowIf from "./components/show-if";
 
 const MainPage = () => {
   const [mobile, setMobile] = useState(false);
   const [data, setData] = useState({});
+  const [loaded, setLoaded] = useState(false);
   const ids = [166039116, 142388943];
   const id = ids[Math.floor(Math.random() * ids.length)];
   const url = `/api/items/active/${id}`;
@@ -21,13 +23,16 @@ const MainPage = () => {
         setMobile(true);
       }
       await getItem(url, setData);
+      setLoaded(true);
     })();
   }, [url]);
 
   return (
     <div className={style.body}>
       <SectionOne mobile={mobile} />
-      <SectionTwo mobile={mobile} data={data} />
+      <ShowIf condition={loaded}>
+        <SectionTwo mobile={mobile} data={data} />
+      </ShowIf>
       <SectionThree mobile={mobile} />
       <SectionFour mobile={mobile} />
       <SectionFive />
